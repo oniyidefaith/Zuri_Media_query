fad92efbd58817e549dba95bec048991fcc74180
oniyidefaith <oniyidefaith30@gmail.com>
2022-06-17 15:07:40 +0100

    update

--- index.js
+++ index.js
@@ -1,22 +1,63 @@
-const first = document.getElementById('first');
-const formField = document.querySelector('form-Field');
+const form = document.getElementById('form');
+const FirstName = document.getElementById('First-Name');
+const lastName = document.getElementById('Last-Name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-function firsterr (){
-    if (first == ""){
-        showError(
-            first,
-            "First Name cannot be empty"
-        );
-    }
-}
-function showError(input, message){
-    const formField = input.parentElement;
-    formField.className = "form-Field error"
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
 
-    if ((formField.className = "form-Field error" )){
-    const alert_message = formField.querySelector(".alert_message");
-    alert_message.style.visibility= "visible";
-    alert_message.style.color = "hsl(0, 100%, 74%)"
-    alert_message.innerText = message;
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = FirstName.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = lastName.value.trim();
+	const password2Value = password.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(FirstName, 'First name cannot be empty');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Looks like this is not an email');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'example@gmail.com');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password, 'Password cannot be empty');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(lastName, 'Last name cannot be empty ');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(lastName, 'Passwords does not match');
+	} else{
+		setSuccessFor(lastName);
+	}
 }
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
